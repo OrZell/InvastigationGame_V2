f1075932f76ad372c9cd492345a44cc0c568e32c
Or Zellinger <or.zellinger@gmail.com>
2025-06-19 17:08:48 +0300

    Complete All the Sensors

---
+++ InvastigationGame_V2/Models/Sensor/SelolarSensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvastigationGame_V2.Models.Sensor
+{
+    public class SelolarSensor : Sensor
+    {
+        public SelolarSensor() : base("selolar")
+        {
+
+        }
+    }
+}
---
+++ InvastigationGame_V2/Models/Sensor/Sensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvastigationGame_V2.Models.Sensor
+{
+    public class Sensor
+    {
+        public string Type;
+        public bool Active;
+
+        public Sensor(string type)
+        {
+            this.Type = type;
+        }
+
+        public virtual void Activate()
+        {
+            this.Active = false;
+        }
+    }
+}
